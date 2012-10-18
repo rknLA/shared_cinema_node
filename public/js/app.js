cfb40c5c67258904c06f8b0c006b8f19e8792183
Chris Abrams <mail@chrisabrams.com>
2012-10-18 00:50:46 -0700

    Cleaned up some things on mobile
    Signed-off-by: Chris Abrams <mail@chrisabrams.com>

--- public/js/app.js
+++ public/js/app.js
@@ -15,89 +15,22 @@
 	$(document).on('pageshow', '#vote', function() {
 
 		$.getJSON('data/videos.json', function(res) {
-
-			var list = $('#video-list');
-			list.html('');
-
-			$.each(res.videos, function(index, video) {
-				//List element
-				var li = document.createElement('li');
-
-				//Anchor element
-				var a  = document.createElement('a');
-				a.href = '#';
-				//a.innerHTML = video.title;
-
-				//Left
-				var left = document.createElement('div');
-				left.className += ' video-list-left';
-
-				//Image
-				var img = document.createElement('img');
-				img.className += ' video-list-img';
-				img.src = video.thumb;
-				left.appendChild(img);
-
-				//Right
-				var right = document.createElement('div');
-				right.className += ' video-list-right';
-
-				//Title
-				var h3 = document.createElement('h3');
-				h3.innerHTML = video.title;
-				right.appendChild(h3);
-
-				//Description
-				var span = document.createElement('span');
-				span.innerHTML = video.desc;
-				right.appendChild(span);
-
-				//Append left and right to anchor
-				a.appendChild(left);
-				a.appendChild(right);
-
-				//Thumb
-				var thumb = document.createElement('button');
-				thumb.className += ' thumb';
-				thumb.type = 'button';
-				thumb.innerHTML = '&nbsp;';
-				a.appendChild(thumb);
-
-				//Append anchor to list
-				li.appendChild(a);
-
-				//Append list element to list
-				list.append(li);
-				//list.append('<li><a href="#">'+video.title+'</a></li>');
-			});
-
-			list.listview('refresh');
-
-			list.on('click', function(e) {
-				e.preventDefault();
-
-				if(e.target.tagName == "BUTTON") {
-					var el = e.target;
-
-					console.log("I am the upvote button");
-				}
-			});
+			renderItems(res);
 		});
 	});
 
 	$(document).on('pageshow', '#search', function() {
-
-		fetchUser(function() {
-
+		fetchUser(function(userID) {
+			setupVideoSearch(userID);
 		});
 	});
 
-	function fetchUser() {
+	function fetchUser(callback) {
 		//Is userID in localstorage?
 		var userID = localStorage.getItem('sc-userID');
 
 		if(userID) {
-			userSuccessResponse(userID);
+			callback(userID); return;
 		} else {
 			$.ajax({
 				url: url + '/users',
@@ -112,15 +45,14 @@
 				success: function(res) {
 					var userID = res._id;
 					localStorage.setItem('sc-userID', userID);
-					userSuccessResponse(userID);
+					if(typeof callback === "function") callback(userID);
 				}
 			});
 		}
 	}
 
-	function successResponse(res) {
-
-		var list = $('#search-list');
+	function renderItems(res, callback) {
+		var list = $('#video-list');
 		list.html('');
 
 		$.each(res.videos, function(index, video) {
@@ -177,18 +109,10 @@
 
 		list.listview('refresh');
 
-		list.on('click', function(e) {
-			e.preventDefault();
-
-			if(e.target.tagName == "BUTTON") {
-				var el = e.target;
-
-				console.log("I am the upvote button");
-			}
-		});
+		if(typeof callback === "function") callback(list);
 	}
 
-	function userSuccessResponse(userID) {
+	function setupVideoSearch(userID) {
 		
 		var timer = null;
 		var $searchInput = $('#search .ui-input-search [data-type=search]');
@@ -208,7 +132,19 @@
 					headers: {
 						Accept: 'application/json'
 					},                                                             
-					success: successResponse,
+					success: function(res) {
+						renderItems(res, function(list) {
+							list.on('click', function(e) {
+								e.preventDefault();
+
+								if(e.target.tagName == "BUTTON") {
+									var el = e.target;
+
+									console.log("I am the upvote button");
+								}
+							});
+						});
+					},
 					error: function(msg) {
 						console.log(msg.responseText)
 					}
